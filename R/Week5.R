ed60c69d93c50b395b2f28c44d5bcf96e17bbb28
quxxx179 <quxxx179@ebm-er-10-131-194-151.wireless.umn.edu>
2020-02-13 15:02:20 -0600

    Simplified the code

--- R/Week5.R
+++ R/Week5.R
@@ -12,21 +12,21 @@ Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
 #Data Cleaning
 library(lubridate)
 Adata_tbl <- Adata_tbl %>%
-  separate(qs, c("q1", "q2", "q3", "q4", "q5"), sep = " - ") %>%
-  mutate_at(c("q1","q2","q3", "q4", "q5"), as.numeric) %>%
+  separate(qs, paste0("q", 1:5), sep = " - ") %>%
+  mutate_at(paste0("q",1:5), as.numeric) %>%
   mutate(datadate = mdy_hms(datadate))
 Aaggr_tbl <- Adata_tbl %>%
-  transmute(parnum, stimver, meanScore = (q1+q2+q3+q4+q5)/5) %>%
+  transmute(parnum, stimver, meanScore = rowMeans(Adata_tbl[,5:9])) %>%
   spread(stimver, meanScore)
 Baggr_tbl <- Bdata_tbl %>% 
-  transmute(parnum, stimver, meanScore = (q1+q2+q3+q4+q5)/5) %>%
+  transmute(parnum, stimver, meanScore = rowMeans(Bdata_tbl[,5:9])) %>%
   spread(stimver, meanScore)
 Aaggr_tbl <- Aaggr_tbl %>% full_join(Anotes_tbl, by = "parnum")
 Baggr_tbl <- Baggr_tbl %>% full_join(Bnotes_tbl, by = "parnum")
 Aaggr_tbl %>% mutate(type = "Alab") %>%
-  bind_rows(Baggr_tbl 
-            %>% mutate(type = "Blab")) %>%
-  filter(is.na(notes) == TRUE)
+  bind_rows(Baggr_tbl %>% mutate(type = "Blab")) %>%
+  filter(is.na(notes) == TRUE) %>%
+  count(type)
 
   
   
